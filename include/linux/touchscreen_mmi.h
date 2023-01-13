7c0a99108b70a1db72cfb98580dc4da6c4455af4
Cosmin Tanislav <demonsingur@gmail.com>
2023-01-14 01:54:20 +0200

    input: touchscreen: touchscreen_mmi: move sleep to queue
    Change-Id: I5a0afa93221e30486787843d059c9fefc35eadfc
    
    input: touchscreen: touchscreen_mmi: move all power logic to queue
    
    Change-Id: Ia2b28f12688626f8fac7c6157f84c25d7a46493f

--- include/linux/touchscreen_mmi.h
+++ include/linux/touchscreen_mmi.h
@@ -391,6 +391,21 @@ enum ts_mmi_panel_event {
 	TS_MMI_EVENT_UNKNOWN
 };
 
+<<<<<<< HEAD:include/linux/touchscreen_mmi.h
+=======
+enum ts_mmi_work {
+	TS_MMI_DO_POWER_ON,
+	TS_MMI_DO_RESUME,
+	TS_MMI_DO_SLEEP,
+	TS_MMI_DO_POWER_OFF,
+	TS_MMI_DO_PS,
+	TS_MMI_DO_REFRESH_RATE,
+	TS_MMI_DO_FPS,
+	TS_MMI_TASK_INIT,
+	TS_MMI_DO_LIQUID_DETECTION,
+};
+
+>>>>>>> 24181c00c4f3 (input: touchscreen: touchscreen_mmi: move sleep to queue):motorola/include/linux/touchscreen_mmi.h
 #define TS_MMI_RESET_SOFT	0
 #define TS_MMI_RESET_HARD	1
 #define TS_MMI_POWER_OFF	0
