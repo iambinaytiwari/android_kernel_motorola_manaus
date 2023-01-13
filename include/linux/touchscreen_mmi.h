c8811692ada66576ae809d13c68142c6584ba6c4
Cosmin Tanislav <demonsingur@gmail.com>
2023-01-14 01:50:59 +0200

    input: touchscreen: touchscreen_mmi: remove resume stop shortcircuit
    Change-Id: I455f7a70f6ca9c4f0f4abf0bd56eabbddab15387

--- include/linux/touchscreen_mmi.h
+++ include/linux/touchscreen_mmi.h
@@ -570,7 +570,6 @@ struct ts_mmi_dev {
 	atomic_t		touch_stopped;
 	enum ts_mmi_pm_mode	pm_mode;
 
-	atomic_t		resume_should_stop;
 	struct delayed_work	work;
 #ifdef CONFIG_TOUCHCLASS_MMI_MTK_CHARGER
 	struct power_supply *psy;
